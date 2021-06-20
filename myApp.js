fd9032b61478254ba575ce7ba8b986c8ec95433f
K-Phanudet <k.phanudet.work@gmail.com>
2021-06-20 13:47:45 +0700

    add middleware for learn about chain middleware function in this case is function for add server time to the request

--- myApp.js
+++ myApp.js
@@ -5,6 +5,15 @@ app.use((req,res,next)=>{
     console.log(`${req.method} ${req.path} - ${req.ip}`)
     next()
 })
+app.get('/now',
+    (req,res,next)=>{
+        req.time = new Date().toString()
+        next()
+    },
+    (req,res)=>{
+        res.json({time:req.time})
+    }
+)
 app.get('/',(req,res)=>{
     res.sendFile(`${__dirname}/views/index.html`)
 })
