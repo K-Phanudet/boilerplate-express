749725b30f6294418877ca5f9ea7d7cbb80f3548
K-Phanudet <k.phanudet.work@gmail.com>
2021-06-20 13:37:43 +0700

    Add middleware in root level for practice about middleware

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 app.use('/public',express.static(__dirname+ '/public'));
+app.use((req,res,next)=>{
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next()
+})
 app.get('/',(req,res)=>{
     res.sendFile(`${__dirname}/views/index.html`)
 })
