3ebfa4c9ce24e63d6714c8d0d97e89c260dcb2ee
K-Phanudet <k.phanudet.work@gmail.com>
2021-06-20 15:49:17 +0700

    add bodyparser to middleware

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+const bodyParser =require('body-parser')
 const name =[]
+app.use(bodyParser.urlencoded({extended:false}))
 app.use('/public',express.static(__dirname+ '/public'));
 app.use((req,res,next)=>{
     console.log(`${req.method} ${req.path} - ${req.ip}`)
