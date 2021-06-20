abde6edd13a0dba79d5bd2109b06f81fb045298f
K-Phanudet <k.phanudet.work@gmail.com>
2021-06-20 12:19:16 +0700

    get message style from env

--- myApp.js
+++ myApp.js
@@ -5,14 +5,10 @@ app.get('/',(req,res)=>{
     res.sendFile(`${__dirname}/views/index.html`)
 })
 app.get('/',(req,res)=>{res.send('Hello Express')})
-// app.get('/json',(req,res)=>res.json({message:process.env.MESSAGE_STYLE}))
 app.get('/json',(req,res)=>{
-    let messageStyle = process.env.MESSAGE_STYLE || 'ignore'
-    let messageCase = {
-        ignore :(msg)=>msg,
-        uppercase :(msg)=>msg.toUpperCase(),
-    }
-    res.json({message:messageCase[messageStyle]("Hello json")})
+    let msg = "Hello json"
+    newMsg = process.env.MESSAGE_STYLE == 'uppercase' ? msg.toUpperCase() : msg
+    res.json({message:newMsg})
 })
 app.get('/json',(req,res)=>res.json({message:"Hello json"}))
 
